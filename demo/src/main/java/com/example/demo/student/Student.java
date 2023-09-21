98ca1741fd5b4d355961e2c33e500dba76df9ec4
Aditya Dhage <adhage93@gmail.com>
2023-09-21 09:39:27 -0700

    Adding Data Access Layer

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,81 +1,98 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity
+@Table
 public class Student {
 
-    private Long id;
-    private String name;
-    private String email;
-    private LocalDate dob;
-    private Integer age;
+  @Id
+  @SequenceGenerator(
+      name = "student_sequence",
+      sequenceName = "student_sequence",
+      allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+  private Long id;
 
-    public Student() {
-    }
+  private String name;
+  private String email;
+  private LocalDate dob;
+  private Integer age;
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
+  public Student() {}
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
+  public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.dob = dob;
+    this.age = age;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Student(String name, String email, LocalDate dob, Integer age) {
+    this.name = name;
+    this.email = email;
+    this.dob = dob;
+    this.age = age;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public LocalDate getDob() {
-        return dob;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
+  public LocalDate getDob() {
+    return dob;
+  }
 
-    public Integer getAge() {
-        return age;
-    }
+  public void setDob(LocalDate dob) {
+    this.dob = dob;
+  }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+  public Integer getAge() {
+    return age;
+  }
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", dob=" + dob +
-                ", age=" + age +
-                '}';
-    }
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{"
+        + "id="
+        + id
+        + ", name='"
+        + name
+        + '\''
+        + ", email='"
+        + email
+        + '\''
+        + ", dob="
+        + dob
+        + ", age="
+        + age
+        + '}';
+  }
 }
---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository repository) {
+    return args -> {
+      Student miriam =
+          new Student(
+              1L,
+              "Miriam Maisal",
+              "miriam.maisal@gmail.com",
+              LocalDate.of(2000, Month.JANUARY, 7),
+              23);
+      Student joel =
+          new Student(
+              "Joel Maisal", "joel.maisal@gmail.com", LocalDate.of(1999, Month.JANUARY, 3), 24);
+
+      repository.saveAll(List.of(miriam, joel));
+    };
+  }
+}
